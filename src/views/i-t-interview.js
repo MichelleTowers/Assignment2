b85e731dceb6c87cec30faaf8a86176faf4c7a32
Michelle Towers <101253117+MichelleTowers@users.noreply.github.com>
2022-04-21 13:54:08 +1000

    Add files via upload

---
+++ src/views/i-t-interview.js
@@ -0,0 +1,248 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import Navbar from '../components/navbar'
+import Banner from '../components/banner'
+import Footer from '../components/footer'
+import './i-t-interview.css'
+
+const ITInterview = (props) => {
+  return (
+    <div className="i-t-interview-container">
+      <Helmet>
+        <title>IT Interview - Landing Page</title>
+        <meta property="og:title" content="IT Interview - Landing Page" />
+      </Helmet>
+      <Navbar rootClassName="navbar-root-class-name4"></Navbar>
+      <Banner rootClassName="banner-root-class-name4"></Banner>
+      <div className="i-t-interview-container1">
+        <h1 className="i-t-interview-mark-murray">
+          <span className="i-t-interview-text">Mark Murray</span>
+          <span className="i-t-interview-text01">​</span>
+        </h1>
+        <h1 className="i-t-interview-job-title">
+          <span className="i-t-interview-text02">
+            RIS PACS Administrator, Clinical Radiographer
+          </span>
+          <span className="i-t-interview-text03">​</span>
+        </h1>
+        <iframe
+          src="https://www.youtube.com/embed/gOmJwBaYIRM"
+          className="i-t-interview-video"
+        ></iframe>
+        <span className="i-t-interview-interview-report">
+          <span>
+            On Thursday, the 7th of April, we had the opportunity to interview
+            the PACS Administrator, Mark Murray.
+          </span>
+          <br></br>
+          <span>
+            This report covers what it is like working as a PACS Administrator
+            and gaining a deeper view of Mark&apos;s early career life, his
+            motivation for exploring technology, and his career path to becoming
+            a PACS administrator. We had a more in-depth conversation regarding
+            the health IT industry. We discussed other industry-related topics
+            such as different interactions, day-to-day tasks, systems,
+            challenges, and what he enjoys the most about his job. Based on our
+            interview with Mark, we have gone more in-depth and researched
+            different career paths, Medical and technical required skills
+            individuals could take to become a PACS administrator.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            Our interviewee Mark Murray is a RIS PACS Administrator and a casual
+            radiographer with the Murrumbidgee Local Health District and
+            Southern NSW Local Health District. According to eHealth, NSW
+            Government
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span>
+            &quot;Radiology information systems and picture archiving and
+            communication systems (RIS-PACS) are electronic solutions used by
+            medical imaging departments to manage their services. They track a
+            patient&apos;s journey from an image request through to diagnosis -
+            storing key information, digital images and results for easy access
+            by treating teams.&quot;
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            Mark discovered his passion for IT very early on in his career as he
+            spent his spare time building and working on computers at home. He
+            started his career as a federally registered health professional and
+            radiographer. His opportunity to make a career in IT came when
+            medical imaging systems were making a transition from film to
+            digital. This period brought on a lot of change and innovation. His
+            organisation needed staff that could get involved in the testing
+            process and conversion of film images to digital files on patient
+            records systems.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            The changes allowed Mark to transition into a role as RIS PACS
+            Application Specialist and then onto his current role as RIS PACS
+            Administrator. He was not required to have formal qualifications in
+            IT but has completed many short courses on IT and aims to complete a
+            short course on SQL to develop his skills further. Becoming a RIS
+            PACS Administrator first required one to be a radiographer first, as
+            the role required specialist domain knowledge in medical imaging to
+            understand and resolve the issues that arise in this position.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            The role of a RIS PACS Administrator is to support the many systems
+            that allow radiographers and radiologists to deliver medical imaging
+            services to patients. The systems listed by Mark included the
+            Patient Management Medical Records System, Imaging System, Patient
+            Information Management System, and the Electronic Medical Records
+            System. These systems are the backbone of all health services
+            operated around the country.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            Mark&apos;s day to day activities varies depending on if there are
+            any outages or project work required at the time. A large part of
+            his time is taking calls and answering tickets received at the
+            service desk. Many of the enquiries are related to error correction.
+            An example of this type of enquiry would be if a radiographer has
+            either taken an x-ray of the left side of a limb instead of the
+            right side or if the x-ray image was mislabeled. Another example
+            will be if the x-ray images have been stored under the incorrect
+            patient file. Mark is often the point of contact to have these
+            errors corrected within the RIS PAC System. Another essential part
+            of his role is training radiographers in using the Radiology
+            Information System (RIS). On an ad-hoc basis, he also is required to
+            deal with system outages. However, this type of issue is often
+            escalated to a state-wide IT Service Desk to be resolved by Server
+            or Network specialists.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            Different interactions are a big part of Mark&apos;s role; as a PACS
+            Administrator, his day-to-day interactions vary from working
+            alongside System users, which are Radiologists, radiographers, and
+            clinical staff; for different issues arising, he also interacts with
+            server administrators and Network Administrators. He also interacts
+            with patients via a patient portal where patients can have easier
+            access to their X-RAYS. Mark mentioned that working in Health IT,
+            there are different experts for different systems; he used an
+            example that if a system error occurs where the system has failed to
+            receive messages, this issue is allocated to the State-wide Service
+            desk by raising a ticket.
+          </span>
+          <br></br>
+          <span>&amp;#8203;</span>
+          <br></br>
+          <span>
+            For some, being on call 24/7 might be a challenge, but as a PACS
+            administrator, Mark&apos;s primary focus is patient care. Being in
+            Health IT for about 30 years every day is not the same as Mark
+            mentioned; every day is a different challenge. As a PACS
+            administrator who covers the Murrumbidgee Local health district and
+            Southern NSW, Mark finds system outages the most challenging as
+            there is a high demand for X Rays from patients. He mentioned that
+            the best way to approach these challenges is to be patient with
+            people and understand their needs during these unexpected outages.
+            Despite the challenges Mark faces every day, he is very passionate
+            and appreciates his work.
+          </span>
+          <br></br>
+          <span>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span>
+            To be a PACS Administrator, you will need to hold a degree in
+            radiologic technology or a field related to healthcare, such as
+            healthcare administration. Mark mentioned it is best to have a
+            strong interest in IT as there are different interacting systems
+            that one needs to get familiar with within this role. His role
+            connects to the medical field as it requires prior experience in the
+            medical industry. He mentioned that you would have basic knowledge
+            of how the systems work as a radiographer. However, as a PACS
+            Administrator, your knowledge of the systems goes much deeper to
+            maintain, manage and error control the systems. As a PACS
+            Administrator, it is crucial to gain prior experience, preferably as
+            a radiologist or radiographer.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            Based on our research, there are many career paths to becoming a
+            PACS administrator, but some essential skills are required for this
+            role. One of the main requirements is holding a degree in a field
+            related to healthcare, such as a bachelor&apos;s degree in medical
+            imaging, radiography, or applied science. The other aspect of a PACS
+            Administrator is the technical skills required to have deep
+            knowledge of the industry, programs, and systems such as Health
+            Level 7 (HL7), Digital imaging and communications (DICOM) in
+            medicine. Mark also mentioned that it is beneficial to learn SQL
+            because it is used in database systems. Besides the technical and
+            medical skills required for this job, communication skills are a
+            crucial point when dealing with patients, vendors, and providers, as
+            they should be able to explain incredibly high technical
+            information. This role requires a niche skillset and a broad number
+            of challenges, but as Mark mentioned, it is one of the best roles he
+            has been in, and he very much enjoys his current role.
+          </span>
+          <br></br>
+          <span></span>
+        </span>
+      </div>
+      <Footer rootClassName="footer-root-class-name1"></Footer>
+    </div>
+  )
+}
+
+export default ITInterview
